355ed7c42e411271a0672e701b5d12ce4013aabe
citixenken <kmuyesu@gmail.com>
2022-03-12 10:17:03 +0300

    first commit

---
+++ MyContacts/MyContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyContactsApp.swift
+//  MyContacts
+//
+//  Created by Ken Muyesu on 12/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
